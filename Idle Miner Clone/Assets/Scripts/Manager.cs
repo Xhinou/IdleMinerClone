8b999891e19f1d654d6cabcba930c382b0b5d653
AYMERIC-PC\admin <xhin_12@hotmail.fr>
2018-06-25 16:16:36 +0200

    View scroller

---
+++ Idle Miner Clone/Assets/Scripts/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+   // public NameList firstNames;
+    //public NameList surnames;
+
+    public new string name;
+
+    public Manager()
+    {
+        name = GenerateName();
+    }
+
+    private string GenerateName()
+    {
+        return "";
+    }
+}
---
+++ Idle Miner Clone/Assets/Scripts/Mine.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    public int mineId;
+    public int lvl;
+    public int nrMiners;
+    public float minerCapacity;
+    public float totalExtract;
+    public int walkSpeed;
+    public float miningSpeed;
+    public float goldDeposit;
+    public bool hasManager;
+
+    public Mine( int id )
+    {
+        mineId = id;
+        lvl = 1;
+        nrMiners = 1;
+        walkSpeed = 1;
+        goldDeposit = 0;
+        minerCapacity = CalculateMinerCapacity();
+        miningSpeed = CalculateMiningSpeed();
+        hasManager = false;
+    }
+
+    private float CalculateUpgradeCost()
+    {
+        return 0.0f;
+    }
+
+    private float CalculateMinerCapacity()
+    {
+        return 0.0f;
+    }
+
+    private float CalculateMiningSpeed()
+    {
+        return 0.0f;
+    }
+}
